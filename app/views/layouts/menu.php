351710634c1bf56200197d3b11074822ffbb4f94
Jess21fr <63249333+Jess21fr@users.noreply.github.com>
2026-05-27 16:17:59 +0200

    2605271617

--- app/views/layouts/menu.php
+++ app/views/layouts/menu.php
@@ -1,9 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top">
     <div class="container-fluid">
 
         <!-- LOGO -->
-        <a class="navbar-brand d-flex align-items-center" href="/public/index.php">
-            <img src="/public/assets/img/comics_crypt_text_logo.png" alt="ComicsCrypt" height="50" class="me-2">
+        <a class="navbar-brand d-flex align-items-center" href="/comics_crypt/public/index.php">
+            <img src="/comics_crypt/public/assets/img/comics_crypt_text_logo.png"
+                 alt="ComicsCrypt"
+                 height="50"
+                 class="me-2">
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenu">
@@ -14,61 +17,102 @@
 
             <ul class="navbar-nav me-auto">
 
-                <!-- GESTION -->
+                <!-- ========================= -->
+                <!-- GESTION                   -->
+                <!-- ========================= -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
+                    <a class="nav-link dropdown-toggle menu-title" href="#" data-bs-toggle="dropdown">
                         <i class="fa-solid fa-gears"></i> Gestion
                     </a>
 
                     <ul class="dropdown-menu dropdown-menu-dark">
 
-                        <!-- ÉDITEURS -->
                         <li class="dropdown-submenu">
                             <a class="dropdown-item dropdown-toggle" href="#">Éditeurs</a>
                             <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="/editeurs/list">Liste / Gestion</a></li>
-                                <li><a class="dropdown-item" href="/editeurs/create">Créer</a></li>
-                                <li><a class="dropdown-item" href="/editeurs/import">Importer</a></li>
+                                <li><a class="dropdown-item" href="#">Liste / Gestion</a></li>
+                                <li><a class="dropdown-item" href="#">Créer</a></li>
+                                <li><a class="dropdown-item" href="#">Importer</a></li>
                             </ul>
                         </li>
 
-                        <!-- GAMMES -->
                         <li class="dropdown-submenu">
                             <a class="dropdown-item dropdown-toggle" href="#">Gammes</a>
                             <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="/gammes/list">Liste / Gestion</a></li>
-                                <li><a class="dropdown-item" href="/gammes/create">Créer</a></li>
-                                <li><a class="dropdown-item" href="/gammes/import">Importer</a></li>
+                                <li><a class="dropdown-item" href="#">Liste / Gestion</a></li>
+                                <li><a class="dropdown-item" href="#">Créer</a></li>
+                                <li><a class="dropdown-item" href="#">Importer</a></li>
                             </ul>
                         </li>
 
-                        <!-- UNIVERS -->
                         <li class="dropdown-submenu">
                             <a class="dropdown-item dropdown-toggle" href="#">Univers</a>
                             <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="/univers/list">Liste / Gestion</a></li>
-                                <li><a class="dropdown-item" href="/univers/create">Créer</a></li>
-                                <li><a class="dropdown-item" href="/univers/import">Importer</a></li>
+                                <li><a class="dropdown-item" href="#">Liste / Gestion</a></li>
+                                <li><a class="dropdown-item" href="#">Créer</a></li>
+                                <li><a class="dropdown-item" href="#">Importer</a></li>
                             </ul>
                         </li>
 
-                        <!-- SÉRIES -->
+                    </ul>
+                </li>
+
+                <!-- ========================= -->
+                <!-- CHRONOLOGIE               -->
+                <!-- ========================= -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle menu-title" href="#" data-bs-toggle="dropdown">
+                        <i class="fa-solid fa-timeline"></i> Chronologie
+                    </a>
+
+                    <ul class="dropdown-menu dropdown-menu-dark">
+
                         <li class="dropdown-submenu">
-                            <a class="dropdown-item dropdown-toggle" href="#">Séries</a>
+                            <a class="dropdown-item dropdown-toggle" href="#">Par Univers</a>
                             <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="/series/list">Liste / Gestion</a></li>
-                                <li><a class="dropdown-item" href="/series/create">Créer</a></li>
-                                <li><a class="dropdown-item" href="/series/import">Importer</a></li>
+                                <li><a class="dropdown-item" href="#">Marvel</a></li>
+                                <li><a class="dropdown-item" href="#">DC</a></li>
+                                <li><a class="dropdown-item" href="#">Indé</a></li>
                             </ul>
                         </li>
 
-                        <!-- ÉPISODES -->
                         <li class="dropdown-submenu">
-                            <a class="dropdown-item dropdown-toggle" href="#">Épisodes</a>
+                            <a class="dropdown-item dropdown-toggle" href="#">Par Série</a>
                             <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="/episodes/list">Liste / Gestion</a></li>
-                                <li><a class="dropdown-item" href="/episodes/create">Créer</a></li>
-                                <li><a class="dropdown-item" href="/episodes/import">Importer</a></li>
+                                <li><a class="dropdown-item" href="#">Spider-Man</a></li>
+                                <li><a class="dropdown-item" href="#">Batman</a></li>
+                                <li><a class="dropdown-item" href="#">Spawn</a></li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </li>
+
+                <!-- ========================= -->
+                <!-- COLLECTION                -->
+                <!-- ========================= -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle menu-title" href="#" data-bs-toggle="dropdown">
+                        <i class="fa-solid fa-books"></i> Collection
+                    </a>
+
+                    <ul class="dropdown-menu dropdown-menu-dark">
+
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item dropdown-toggle" href="#">Mes Séries</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="#">Liste</a></li>
+                                <li><a class="dropdown-item" href="#">Ajouter</a></li>
+                                <li><a class="dropdown-item" href="#">Importer</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item dropdown-toggle" href="#">Mes Épisodes</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="#">Liste</a></li>
+                                <li><a class="dropdown-item" href="#">Ajouter</a></li>
+                                <li><a class="dropdown-item" href="#">Importer</a></li>
                             </ul>
                         </li>
 
@@ -92,7 +136,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
             let submenu = this.nextElementSibling;
 
-            // Ferme les autres sous-menus
             this.closest('.dropdown-menu').querySelectorAll('.dropdown-menu').forEach(function(menu){
                 if(menu !== submenu){
                     menu.classList.remove('show');
